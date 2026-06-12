c1f55772163a1e4b99143d8f735c4b1adc97e061
yasintkcbyk <yasintalhakoc.54@gmail.com>
2026-06-12 18:56:09 +0300

    APK Build

---
+++ frontend/android/app/src/main/java/com/yasin/nextrade/MainActivity.java
@@ -0,0 +1,5 @@
+package com.yasin.nextrade;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,11 +3,18 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'android', 'public']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      'jsx-a11y': jsxA11y,
+    },
+    rules: {
+      ...jsxA11y.configs.recommended.rules,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
